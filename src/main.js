3c0c3eb7898ee5465e060e06edfd8fdf32ed3dfa
Augustus Backup <evilkim25@gmail.com>
2022-11-05 16:42:49 +0800

    Demo完成

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
-var inputElement4 = document.getElementById('videoEnvFile');
-inputElement4.addEventListener('change', handleFiles, false);
-inputElement4.addEventListener('change', handleFilesVideo, false);
+const input1 = document.getElementById('videoUserFile');
+input1.addEventListener('change', handleFiles, false);
+input1.addEventListener('change', handleFilesVideo, false);
 
-var inputElement5 = document.getElementById('imageEnvFile');
-inputElement5.addEventListener('change', handleFiles, false);
-inputElement5.addEventListener('change', handleFilesBase64, false);
+const input2 = document.getElementById('imageUserFile');
+input2.addEventListener('change', handleFiles, false);
+input2.addEventListener('change', handleFilesBase64, false);
 
+const input3 = document.getElementById('videoEnvFile');
+input3.addEventListener('change', handleFiles, false);
+input3.addEventListener('change', handleFilesVideo, false);
+
+const input4 = document.getElementById('imageEnvFile');
+input4.addEventListener('change', handleFiles, false);
+input4.addEventListener('change', handleFilesBase64, false);
+
+// 取檔案資訊
 function handleFiles(e) {
   const fileData = this.files;
-  const textarea = e.target.id + 'Result';
   const data = fileData[0];
-  document.getElementById(textarea).textContent = data;
-  console.log(data);
+  const lastModified = data.lastModified;
+  const name = data.name;
+  const size = data.size;
+  const type = data.type;
+  const textarea = document.getElementById(e.target.id + 'Result');
+  textarea.value = `
+    lastModified: ${lastModified},
+    name: ${name},
+    size: ${size},
+    type: ${type}
+  `;
+  textarea.classList.remove('hidden');
 }
 
 // 轉影片 URL：https://stackoverflow.com/questions/61012790/how-to-read-large-video-files-in-javascript-using-filereader
@@ -25,18 +43,20 @@ function handleFilesVideo(e) {
     let videoBlob = new Blob([new Uint8Array(buffer)], { type: 'video/mp4' });
     let url = window.URL.createObjectURL(videoBlob);
 
-    const videoSrc = e.target.id + 'Video';
-    document.getElementById(videoSrc).src = url;
-    document.getElementById(videoSrc).classList.remove('hidden');
+    const video = document.getElementById(e.target.id + 'Video');
+    video.src = url;
+    video.classList.remove('hidden');
   });
 }
 
 // 轉圖片 Base64
-function handleFilesBase64() {
+function handleFilesBase64(e) {
   const fileData = this.files;
   const reader = new FileReader();
   reader.addEventListener('load', file => {
-    console.log(file.target.result);
+    const img = document.getElementById(e.target.id + 'Image');
+    img.src = file.target.result;
+    img.classList.remove('hidden');
   });
   reader.readAsDataURL(fileData[0]);
 }
